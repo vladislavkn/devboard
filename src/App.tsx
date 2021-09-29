f08d783ae499dd87ef32aa5cb30db3ba6e436c37
Vladislav Knyshov <inova136@gmail.com>
2021-09-29 20:16:22 +0300

    fix: update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
-const App: React.FC = () => null;
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Icon,
+  Typography,
+  CssBaseline,
+  Box,
+} from "@material-ui/core";
+
+const App: React.FC = () => (
+  <Box sx={{ bgcolor: "grey.50", minHeight: "100vh" }}>
+    <CssBaseline />
+    <AppBar
+      position="sticky"
+      elevation={0}
+      sx={{
+        bgcolor: "white",
+        color: "grey.800",
+        borderBottom: 1,
+        borderColor: "grey.300",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Devboard
+        </Typography>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+          <Icon>filter_alt</Icon>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export default App;
