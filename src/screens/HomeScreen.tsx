dba28a2a0b943f6d3d947e9c90f71052b0fd0b1d
Vladislav Knyshov <inova136@gmail.com>
2021-10-05 22:36:22 +0300

    move query logic to separated hook

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,45 +1,17 @@
-import { useQuery } from "@apollo/client";
-import { useRef, useEffect } from "react";
 import Navigation from "../components/Navigation";
 import PostPreviewList from "../components/PostPreviewList";
-import { QueryLastPostsArgs } from "../generated/graphql";
-import queryLastPosts, { QueryLastPosts } from "../queries/queryLastPosts";
+import useLastPosts from "../hooks/useLastPosts";
 
 const HomeScreen: React.FC = () => {
-  const page = useRef<number>(1);
-  const { data, fetchMore, refetch } = useQuery<
-    QueryLastPosts,
-    QueryLastPostsArgs
-  >(queryLastPosts, {
-    variables: {
-      page: page.current,
-      per_page: 4,
-    },
-  });
-
-  const handleFetchMore = () =>
-    fetchMore({
-      variables: {
-        page: ++page.current,
-        per_page: 4,
-      },
-    });
-
-  const handleRefresh = () => {
-    page.current = 1;
-    refetch({
-      page: page.current,
-      per_page: 4,
-    });
-  };
+  const { data, refresh, fetchMore } = useLastPosts(4);
 
   return (
     <>
       <Navigation title="Devboard" />
       <PostPreviewList
         posts={data?.lastPosts ?? []}
-        onFetchMore={handleFetchMore}
-        onRefresh={handleRefresh}
+        onFetchMore={fetchMore}
+        onRefresh={refresh}
       />
     </>
   );
