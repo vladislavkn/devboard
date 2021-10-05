8de03d2462899c40f75bcdf42dd76357bfb81f38
Vladislav Knyshov <inova136@gmail.com>
2021-10-06 01:09:02 +0300

    update TagScreen: add search & tags fetching

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -12,7 +12,6 @@ import {
   Typography,
   IconButton,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
 import { Post } from "../generated/graphql";
 
 type PostPreviewProps = {
