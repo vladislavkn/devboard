5755547867e7f2dcbaa66666b26dd7203ec22096
Vladislav Knyshov <inova136@gmail.com>
2021-10-10 12:04:26 +0300

    add id filtering for paged queries

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,4 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Post, Tag } from "./generated/graphql";
+
+// @ts-ignore
+window.__env__ = import.meta.env;
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
@@ -8,7 +12,29 @@ const client = new ApolloClient({
         fields: {
           lastPosts: {
             keyArgs: false,
-            merge(existing = [], incoming) {
+            merge(existing: Post[] = [], incoming: Post[], { readField }) {
+              const existingIdSet = new Set(
+                existing.map((post) => readField("id", post))
+              );
+
+              incoming = incoming.filter(
+                (post) => !existingIdSet.has(readField("id", post))
+              );
+
+              return [...existing, ...incoming];
+            },
+          },
+          tags: {
+            keyArgs: false,
+            merge(existing: Tag[] = [], incoming: Tag[], { readField }) {
+              const existingIdSet = new Set(
+                existing.map((tag) => readField("id", tag))
+              );
+
+              incoming = incoming.filter(
+                (tag) => !existingIdSet.has(readField("id", tag))
+              );
+
               return [...existing, ...incoming];
             },
           },
